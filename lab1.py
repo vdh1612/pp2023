bcce6a45f80e9f86655651582d8a889921b76da1
unknown <duchieuvu2003@gmail.com>
2023-02-22 21:25:10 +0700

    new

--- lab1.py
+++ lab1.py
@@ -22,18 +22,24 @@ def list_course():
         print("Course id:"+cid_lst[i])
         print("Name of the course:"+course_name_lst[i])
 mark_lst = []
+course_name_choice = []
 def input_mark():
     cid = input("Enter the id of the course that you want to mark:")
     if cid not in cid_lst:
         print("No course was found!")   
         return 0
+    print("Enter the name of the course has id",cid,":",end = " ")
+    course_name = input()
+    if course_name not in course_name_lst:
+        print("Error name of the course !")
+        return 0
+    course_name_choice.append(course_name)    
     for name in name_students:
         print("please enter the mark for",name,":",end = " ")
         mark = int(input())
         mark_lst.append(mark)
-    if sid not in sid_lst:
-        print("The student does not exist!")           
 def display_mark():
+    print("In the",course_name_choice[0],"course:",end = "\n")
     for i in range(0,len(name_students)):
         print("{0}'s mark is:{1}".format(name_students[i],mark_lst[i]))
 num_student = number_student()
